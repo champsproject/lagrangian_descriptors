fc601cc6611f41323a68a4b7d5d82f647449b24f
Broncio <ba13026@my.bristol.ac.uk>
2020-07-15 11:57:32 +0100

    Build No 3, replaced minibox and itemize latex env  with Markdown syntax

--- book/_build/jupyter_execute/content/chapter1_2.py
+++ book/_build/jupyter_execute/content/chapter1_2.py
@@ -4,10 +4,12 @@
 ## Hamiltonian flows
 
 A Hamiltonian system with $N$ degrees of freedom is described by the Hamiltonian function $H(\mathbf{q},\mathbf{p},t)$, which depends on the generalized coordinates $\mathbf{q} = (q_1,\ldots,q_N)$, their canonically conjugate momenta $\mathbf{p} = (p_1,\ldots,p_N)$, and time. This scalar function gives rise to a $2N$-dimensional dynamical system by means of Hamilton's equations of motion
+
 \begin{equation}
 \dot{q}_i = \frac{\partial H}{\partial p_i} \quad,\quad \dot{p}_i = -\frac{\partial H}{\partial q_i} \;, \quad \text{for} \; i = 1, 2, \ldots, N. \label{eqn:hamiltons_eom}
 \end{equation}
 where the dot symbol represents the total time derivative, that is,  $\cdot \equiv d/dt$. A state of the system is represented by a point in the $2N$-dimensional \emph{phase space}, Any solution of this system is a trajectory of the form
+
 \begin{equation}
 \mathbf{x}(t) = \left(q_1(t), \ldots, q_N(t), p_1(t), \ldots, p_N(t)\right) \,, 
 \label{eqn:solution}
@@ -22,12 +24,11 @@ We will assume that $H$ is sufficiently smooth to guarantee the uniqueness of so
 \phi_s(\mathbf{x}(t))=\mathbf{x}(s+t) \;. \label{eqn:flow}
 \end{equation}
 The family is a group because it satisfies the properties
-\begin{itemize}
- \item $\phi_0=Id$,
- \item $\phi_s\circ \phi_t=\phi_{t+s}$,
- \item $\phi_{-t} \circ \phi_t=\phi_0$.
-\end{itemize}
- 
+
+* $\phi_0=Id$,
+* $\phi_s\circ \phi_t=\phi_{t+s}$,
+* $\phi_{-t} \circ \phi_t=\phi_0$.
+
 
 ## Symplectic maps
 A linear transformation $C$ is symplectic if it satisfies
@@ -155,67 +156,68 @@ An important feature of canonically conjugate pairs lies in their role in defini
 
 The phase space structures that govern the dynamics of a Hamiltonian system with $N$ degrees of freedom are of codimension-$1$ and codimension-$2$ in $2N$-dimensional phase space, in the case of an autonomous system codimension-$1$ and codimension-$2$ in the $2N-1$-dimensional energy surface \cite{Waalkens08} (refer to hyperbolic section). A Poincar\'e surface of section is a $(2N - 2)$-dimensional surface, for example one specified by $q_j = K$ with $\dot{q}_j \geq 0$ or $\dot{q}_j \leq 0$,
 where $j \in 1, 2, \ldots, N$ and $K$ is constant. For this Poincar\'e surface of section to be transverse to the flow, we require
+
 \begin{equation}
 \dot{q}_j = \dfrac{\partial H}{\partial p_j} \neq 0 \, ,
 \end{equation}
 on the entire surface of section.
 
 We note here that the complete $(2N - 2)$-dimensional surface cannot be visualized without projecting onto a $2$ or $3$ dimensional space. Even a three dimensional projection can be difficult to visualize due to the limitation of the viewing medium such as the screen or paper. However, if we consider a two-dimensional slice of the $2N-1$ dimensional energy surface, then the intersection of the slice with a codimension-$1$ or codimension-$2$ object is either one- or zero-dimensional provided they intersect. This follows  the rule 
-$$\text{dimension of intersection = (dimension of surface + dimension of object) - dimension of the space.}$$
+
+\begin{equation*}
+\text{dimension of intersection = (dimension of surface + dimension of object) - dimension of the space.}
+\end{equation*}
+
 Visualising one- or zero-dimensional intersections on a two-dimensional surface makes high-dimensional phase space structures accessible again. This visualization may not give us the whole picture of these structures in high-dimensional phase space but it is useful in order to obtain important dynamical information about these high-dimensional structures, for example to detect the intersections of the invariant manifolds and many others.
 
 
 ## Liouville's Theorem
 
-\fbox{%
-\begin{minipage}{\linewidth}
-This subsection and concepts herein are adapted from \emph{Classical Mechanics by Goldstein, 3rd edition}.
-\end{minipage}
-}
+> This subsection and concepts herein are adapted from \emph{Classical Mechanics by Goldstein, 3rd edition}.
 
 Suppose some property of the system is denoted by $A = A(\mathbf{x}, t)$, then the total time rate of change of this property along the solutions of an autonomous Hamiltonian system $H$ is 
+
 \begin{equation}
 \frac{d A}{d t} = \frac{\partial A}{\partial t} + \sum\limits_{i=1}^{N} \left( \frac{\partial A}{\partial q_i}\dot{q}_i + \frac{\partial A}{\partial p_i} \dot{p}_i \right) = \frac{\partial A}{\partial t} + \sum\limits_{i=1}^{N} \left( \frac{\partial A}{\partial q_i}\frac{\partial H}{\partial p_i} - \frac{\partial A}{\partial p_i} \frac{\partial H}{\partial q_i} \right)
 \end{equation}
+
 Using the definition of the \emph{Poisson bracket}:
+
 \begin{equation}
 \{A, H\} = \sum\limits_{i=1}^{N} \left( \frac{\partial A}{\partial q_i}\frac{\partial H}{\partial p_i} - \frac{\partial A}{\partial p_i} \frac{\partial H}{\partial q_i} \right)
 \end{equation}
+
 we can write the time evolution of the property as
+
 \begin{equation}
 \frac{d A}{d t} = \frac{\partial A}{\partial t} + \{A, H\}
 \label{eqn:property_conserved}
 \end{equation}
+
 Therefore, if we assume that the Hamiltonian does not depend explicitly on time and use $A = H$ in the above equation, we get
+
 \begin{equation}
 \frac{d H}{d t} = 0 + \{H, H\} = 0
 \label{eqn:energy_conserved}
 \end{equation}
+
 which implies that the Hamiltonian, $H$, is a constant of the motion for conservative systems, that is the total energy is fixed. Thus, the states for a specified energy are restricted to the $(2N - 1)$-dimensional surface embedded in the $2N$-dimensional phase space.
 
 We use this set-up to present the mathematical statement of the Liouville's theorem which states: \emph{when a classical ensemble evolves its location and shape in phase space may change but its volume will be conserved}.
 
 A classical ensemble consists of a set of points in phase space, with each point representing a system in a specified state. Let $\rho(\mathbf{x},t)$ be the density of the set of points in a given volume. As each member of ensemble moves through phase space along a trajectory specified by Hamilton's equations of motion, the phase space density evolves in time.
 
-
-
 Using Poisson brackets \eqref{eqn:property_conserved}, we can express Liouville's theorem as
+
 \begin{equation}
 \frac{d \rho}{d t} = \frac{\partial \rho}{\partial t} + \{\rho, H\} = 0,
 \end{equation}
+
 which means that the phase space density $\rho(\mathbf{x}, t)$ is conserved along the solutions of $H$. Consequently when an ensemble evolves, its location and shape in phase space change but its volume is conserved. It also follows that all autonomous Hamiltonian systems are volume preserving due to Liouville's theorem.
-
-
 
 ## Poincar\'e recurrence
 
-\fbox{%
-\begin{minipage}{\linewidth}
-This section is adapted from the book \emph{Applied Nonlinear Dynamical systems by S. Wiggins (Springer, 2003)} and the review article \emph{Symplectic maps, variational principles, and transport by J.D. Meiss (Rev. Mod. Phys. 1992)}.
-\end{minipage}
-}
-
-\vspace{.5cm}
+> This section is adapted from the book <em>Applied Nonlinear Dynamical systems by S. Wiggins </em> (Springer, 2003) and the review article <em>Symplectic maps, variational principles, and transport by J.D. Meiss </em> (Rev. Mod. Phys. 1992).
 
 Hamiltonian systems with a bounded energy surface have a recurrence property discovered by Henri Poincar\'e (1890). The Poincar\'e recurrence theorem states that every state of a system will return arbitrarily close to its initial state in finite (potentially very long) time. 
 
@@ -226,5 +228,4 @@ If the energy surface is bounded (compact), then the Poincar\'e recurrence theor
 Combining Liouville's theorem and Poincar\'e recurrence theorem leads to the conclusion, that a set of points of a given volume on a Poincar\'e surface of section will return to the surface and intersect it in a set of points of equal volume. The return map associated with this surface is therefore volume preserving.
 
 
-As pointed out by Arnold \cite[p. 71]{Arnold76}, ``The following prediction is a paradoxical conclusion from the theorems of Poincar\'e and Liouville: if you open a partition separating a chamber containing gas and a chamber with a vacuum, then after a while the gas molecules will again collect in the first chamber. The resolution of the paradox lies in the fact that ``a while  may be longer than the duration of the solar system's existence''. 
-
+As pointed out by Arnold \cite{Arnold76}, _"The following prediction is a paradoxical conclusion from the theorems of Poincar\'e and Liouville: if you open a partition separating a chamber containing gas and a chamber with a vacuum, then after a while the gas molecules will again collect in the first chamber. The resolution of the paradox lies in the fact that"_ a while  may be longer than the duration of the solar system's existence.
